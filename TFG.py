1731a1f157fd6b94151c4365cc6fa9ba9dee5711
Ines <115102810+inessallent@users.noreply.github.com>
2025-01-24 18:16:32 +0100

    Update TFG.py

--- TFG.py
+++ TFG.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import base64 
+
+# Función para establecer el fondo
+
+# def set_background(image_path):
+#     with open(image_path, "rb") as image_file:
+#         base64_image = base64.b64encode(image_file.read()).decode()
+#     background_style = f"""
+#     <style>
+#     .stApp {{
+#         background-image: url("data:image/png;base64,{base64_image}");
+#         background-size: cover;
+#         background-position: center;
+#     }}
+#     </style>
+#     """
+#     st.markdown(background_style, unsafe_allow_html=True)
+
+# # Establecer el fondo
+# set_background("C:/Users/isall/OneDrive/UNI/TFG/TFG/fondo.png")
 
 
 # Title App 
@@ -6,21 +26,21 @@ st.title("TFG") #Redefinir
 
 # Sidebar para la navegación
 st.sidebar.title("Navegación") 
-page = st.sidebar.selectbox("Selecciona una sección:", ["Inicio", "Cuestionario", "Sobre Nosotros", "Contacto"])
+page_web = st.sidebar.selectbox("Selecciona una sección:", ["Inicio", "Questionario", "Sobre Nosotros", "Contacto"])
 
 # Cargar una imagen en la página inicial
-if page == "Inicio":
-    #st.image("ruta/a/tu/imagen.jpg", caption="Descripción de la imagen", use_column_width=True)
+if page_web == "Inicio":
+    st.image("C:/Users/isall/OneDrive/UNI/TFG/TFG/fondo.png", caption="Descripción de la imagen", use_column_width=True)
     st.write("Aquí puedes escribir información sobre quiénes son.")
 
-elif page == "Cuestionario":
+elif page_web == "Questionario":
     st.write("Aquí puedes escribir información sobre quiénes son.")
     
-elif page == "Sobre Nosotros":
+elif page_web == "Sobre Nosotros":
     st.write("This questionnaire is part of the final project from my degree.")
 
 
-elif page == "Contacto":
+elif page_web == "Contacto":
     st.write("Si deseas ponerte en contacto con nosotros, por favor completa el siguiente formulario:")
     email = st.text_input("Tu correo electrónico:")
     mensaje = st.text_area("Tu mensaje:")
