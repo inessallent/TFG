3833ff2d9e5f14197b0dc378b12bd8e014c5c9d1
Ines <115102810+inessallent@users.noreply.github.com>
2025-01-30 16:24:10 +0100

    Update TFG.py

--- TFG.py
+++ TFG.py
@@ -7,16 +7,18 @@ import base64
 
     
 def main():  
+    
     # Sidebar para la navegación
-    st.sidebar.title("Navegación") 
-    st.sidebar.image("UPF_logo.png")
+    st.sidebar.title("Navegación")    #Title 
+    st.sidebar.image("UPF_logo.png")  #Upload Logo
     page_web = st.sidebar.selectbox("Selecciona una sección:", ["Inicio", "Questionario", "Sobre Nosotros", "Contacto"])
 
-    # Cargar una imagen en la página inicial
+    #Init 
     if page_web == "Inicio":
         st.title('Inicio')
         st.write("Aquí puedes escribir información sobre quiénes son.")
 
+    #Questionario
     elif page_web == "Questionario":
         st.title('Questionario')
         st.write("Aquí puedes escribir información sobre quiénes son.")
@@ -24,12 +26,13 @@ def main():
         st.header("Question 1:")
         answer_1 = st.radio("What would you choose?", ("1", "2", "3")) 
         button1 = st.button("Submit Answer")
-        
+    
+    #Sobre Nosotros   
     elif page_web == "Sobre Nosotros":
         st.title('Sobre Nosotros')
         st.write("This questionnaire is part of the final project from my degree.")
 
-
+    #Contacto 
     elif page_web == "Contacto":
         st.write("Si deseas ponerte en contacto con nosotros, por favor completa el siguiente formulario:")
         email = st.text_input("Tu correo electrónico:")
@@ -38,7 +41,7 @@ def main():
             st.success("¡Mensaje enviado con éxito!")
 
 
-    # style_metric_cards(border_left_color="#e1ff8b",background_color="#222222")
+    style_metric_cards(border_left_color="#e1ff8b",background_color="#222222")
 
 if __name__ == "__main__":
     main()
