129ec78f09cb56b5e380ea944f1f7c258fce6516
Ines <115102810+inessallent@users.noreply.github.com>
2025-01-30 16:20:29 +0100

    UPDATE

--- TFG.py
+++ TFG.py
@@ -1,6 +1,8 @@
+
+
 import streamlit as st # type: ignore
 import pandas as pd # type: ignore 
-from streamlit_extras.metric_cards import style_metric_cards
+from streamlit_extras.metric_cards import style_metric_cards # type: ignore 
 import base64 
 
     
