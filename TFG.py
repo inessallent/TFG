05815bb0b787539a8024fd29577dcbd7f9ad23f7
Ines <115102810+inessallent@users.noreply.github.com>
2025-01-30 19:00:47 +0100

    Update TFG.py

--- TFG.py
+++ TFG.py
@@ -43,7 +43,7 @@ def next_video(question_index):
 
 # Display Question
 def display_question(questions):
-    current_question = questions[st.session_state.question_index - 2]  # -1 because index 0 is for personal info
+    current_question = questions[st.session_state.question_index - 1]  # -1 because index 0 is for personal info
     
     # Mostrar el video 
     next_video(st.session_state.question_index -1)
