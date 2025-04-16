475decc07c289d92f14dbef7fc739eb9406ae2c5
Ines <115102810+inessallent@users.noreply.github.com>
2025-04-16 17:22:51 +0200

    Update diccionarios

--- idiomas/catala.py
+++ idiomas/catala.py
@@ -4,55 +4,69 @@ import streamlit as st
 
 textos = {
     "seleccionar_idioma": "Selecciona l'idioma",    
+    
+    # Opcions preguntes
     "totalmente_en_desacuerdo": "Totalment en desacord",
     "en_desacuerdo": "En desacord",
     "neutral": "Neutral",
     "de_acuerdo": "D'acord",
     "totalmente_de_acuerdo": "Totalment d'acord",
-    "video_no_encontrado": "El vídeo no s'ha trobat a la ruta especificada.",
-    "selecciona_opción": "Si us plau, selecciona una opció abans de continuar.",
-    "cuestionario": "Questionari",    
-    "nombre": "Nom:", 
-    "info_personal": "Informació Personal", 
-    "apellido": "Cognom:", 
+    
+    "cuestionario": "Qüestionari",    
+    
+    "boton_continuar": "Continuar", 
+    "error_correo": "Si us plau, introdueix un correu electrònic vàlid.",
+    "enviado_con_éxtio": "Enviat amb èxit!",
+    "Seccion_1": "Secció 1",
+    "Seccion_2": "Secció 2",
+    "Seccion_3": "Secció 3",
+    
+    # Preguntes Secció 1: Informació Personal 
+    "información_personal": "Informació personal",
+    "info_personal": "Informació personal", 
     "pregunta_genero": "Gènere:",
     "genero_opciones": ["Femení", "Masculí", "No binari", "Prefereixo no dir-ho"],
     "pregunta_edad": "Edat:",
-    "edad_opciones": ["Menys de 18", "18 - 24", "25 - 34", "35 - 44", "45 - 54", "55 - 64", "Més de 64", "Prefereixo no dir-ho"],
-    "opcion_correo": "Correu Electrònic (opcional):", 
-    "boton_continuar": "Continuar", 
-    "error_correo": "Si us plau, introdueix una adreça de correu electrònic vàlida.",
-    "enviado_con_éxtio": "Enviat amb èxit!",
-    "advertencia_faltan_datos": "Si us plau, introdueix el teu nom, cognom, gènere i edat. Gràcies.", 
-    
-    #Preguntes secció 1: Informació Personal
-    "información_personal": "Informació Personal",
-    "pregunta_nivel_estudio": "Nivell d'estudis",
+    "edad_opciones": ["Menor de 18", "18 - 24", "25 - 34", "35 - 44", "45 - 54","55 - 64", "Més de 64", "Prefereixo no dir-ho"],
+    "opcion_correo": "Correu electrònic (opcional):", 
+    "pregunta_nivel_estudios": "Nivell d'estudis",
     "opciones_nivel_estudios": ["Estudis primaris", "Estudis secundaris", "Estudis universitaris (o superiors als secundaris)", "Sense estudis", "Altres"], 
-    "pregunta_sector_estudio_trabajo": "Si us plau, selecciona el sector que millor descrigui la teva feina o estudis (en el cas que siguis estudiant):", 
-    "opciones_sector": ["Arts i Humanitats", "Negocis i Economia", "Ciències de la Computació i Informàtica", "Educació", "Enginyeria i Tecnologia", "Ciències Ambientals i de la Terra", "Salut i Medicina", "Dret i Estudis Legals", "Ciències de la Vida i Biologia", "Matemàtiques i Estadística", "Ciències Físiques (p. ex., Física, Química)", "Ciències Socials", "Altres"],
-    "pregunta_experiencia": "Quants anys d'experiència tens en aquest àmbit?:", 
-    "opciones_experiencia": ["Menys d'1 any", "1 - 3 anys", "4 - 6 anys", "7 - 10 anys", "Més de 10 anys"], 
-    "pregunta_ciudad": "¿En que païs resideix?",
-    "Gracias_por_contestar_el_formulario": "Moltes gràcies per contestar el formulari!",
-
+    "pregunta_rama_estudios" : "Si us plau, selecciona la branca que millor descrigui els teus estudis:", 
+    "opciones_rama_estudios": [
+        "Arts i Humanitats (Disseny, Filosofia, Història, Traducció i Interpretació, ...)",
+        "Ciències (Ciències Ambientals, Física, Geologia, Matemàtiques, Química, ...)",
+        "Ciències de la Salut (Infermeria, Medicina, Psicologia, Odontologia, Veterinària, ...)",
+        "Ciències Socials i Jurídiques (ADE, Comunicació Audiovisual, Criminologia, Dret, Economia, Periodisme, Turisme, ...)",
+        "Biociències (Biologia, Bioquímica, Genètica, Microbiologia, ...)",
+        "Enginyeries i Arquitectura (Enginyeria, Industrials, Informàtica, Telecomunicacions, ...)",
+        "Altres"
+    ],
+    "pregunta_años_experiencia": "Quants anys d'experiència tens en aquest àmbit?:", 
+    "opciones_años_experiencia": ["Menys d'1 any", "1 - 3 anys", "4 - 6 anys", "7 - 10 anys", "Més de 10 anys"],  
+    "pregunta_pais_residencia": "En quin país resideixes?",
+    "Gracias_por_contestar_el_formulario": "Moltes gràcies per respondre el formulari!",
     
-    # Texts main
+    # Textos main
     "navegacion": "Navegació", 
     "selecciona_una_seccion": "Selecciona una secció:", 
     "contacto": "Contacte", 
-    "sobre_nosotros": "Sobre Nosaltres",
-    "info_sobre_nosotros": "Aquest qüestionari és part del projecte final de la meva carrera.", 
-    "info_contacto": "Si desitges posar-te en contacte amb nosaltres, si us plau completa el següent formulari:", 
-    "correo_electronico_contacto": "Correu electrònic: ", 
-    "mensaje": "Missatge: ", 
+    "sobre_nosotros": "Sobre nosaltres",
+    "info_sobre_nosotros": "Aquest qüestionari forma part del projecte final del meu grau.", 
+    "info_contacto": "Si vols posar-te en contacte amb nosaltres, si us plau completa el següent formulari:", 
+    "correo_electronico_contacto": "Correu electrònic:", 
+    "mensaje": "Missatge:", 
     "enviar": "Enviar", 
-    "mensaje_enviado": "Missatge enviat amb èxit!",  
+    "mensaje_enviado": "Missatge enviat amb èxit!", 
     
-    #Errores
-    "error_nombre": "Si us plau, introdueixi el seu nom. ",  
-    "error_apellido": "Si us plau, introdueixi el seu cognom. ",  
-    "error_genero": "Si us plau, introdueixi el seu gènere. ",  
-    "error_correo": "Correu electrònic no vàlid. ",  
-    "error_age": "Si us plau, introdueixi la seva edat. ",  
+    # Errors
+    "error_genero": "Si us plau, introdueix el teu gènere.",
+    "error_correo": "Correu electrònic no vàlid.",
+    "error_age": "Si us plau, introdueix la teva edat.",
+    "error_nivel_estudios": "Si us plau, introdueix el teu nivell d'estudis.",
+    "error_rama_estudios": "Si us plau, introdueix la teva branca d'estudis.", 
+    "error_años_experiencia": "Si us plau, introdueix quants anys d'experiència tens en el teu àmbit d'estudis.",
+    "error_pais_residencia": "Si us plau, introdueix el teu país de residència.", 
+    
+    "video_no_encontrado": "El vídeo no s'ha trobat a la ruta especificada.",
+    "selecciona_opción": "Si us plau, selecciona una opció abans de continuar.",
 }
--- idiomas/english.py
+++ idiomas/english.py
@@ -1,55 +1,70 @@
 import streamlit as st
 
 textos = {
-    "seleccionar_idioma": "Select Language",    
-    "totalmente_en_desacuerdo": "Strongly Disagree",
+    "seleccionar_idioma": "Select language",
+    
+    # Question options
+    "totalmente_en_desacuerdo": "Strongly disagree",
     "en_desacuerdo": "Disagree",
     "neutral": "Neutral",
     "de_acuerdo": "Agree",
-    "totalmente_de_acuerdo": "Strongly Agree",
-    "video_no_encontrado": "The video was not found at the specified path.",
-    "selecciona_opción": "Please select an option before continuing.",
+    "totalmente_de_acuerdo": "Strongly agree",
+
     "cuestionario": "Questionnaire",    
-    "nombre": "First Name:", 
+
+    "boton_continuar": "Continue",
+    "error_correo": "Please enter a valid email address.",
+    "enviado_con_éxtio": "Successfully sent!",
+    "Seccion_1": "Section 1",
+    "Seccion_2": "Section 2",
+    "Seccion_3": "Section 3",
+
+    # Section 1: Personal Information
+    "información_personal": "Personal Information",
     "info_personal": "Personal Information", 
-    "apellido": "Last Name:", 
     "pregunta_genero": "Gender:",
     "genero_opciones": ["Female", "Male", "Non-binary", "Prefer not to say"],
     "pregunta_edad": "Age:",
     "edad_opciones": ["Under 18", "18 - 24", "25 - 34", "35 - 44", "45 - 54", "55 - 64", "Over 64", "Prefer not to say"],
-    "opcion_correo": "Email (optional):", 
-    "boton_continuar": "Continue", 
-    "error_correo": "Please enter a valid email address.",
-    "enviado_con_éxtio": "Sent successfully!",
-    "advertencia_faltan_datos": "Please enter your first name, last name, gender, and age. Thank you.", 
-    
-    # Questions section 1: Personal Information
-    "información_personal": "Personal Information",
-    "pregunta_nivel_estudio": "Education Level",
-    "opciones_nivel_estudios": ["Primary Education", "Secondary Education", "University Education (or higher than secondary)", "No Education", "Others"],
-    "pregunta_sector_estudio_trabajo": "Please select the sector that best describes your work or studies (if you are a student):", 
-    "opciones_sector": ["Arts and Humanities", "Business and Economics", "Computer Science and IT", "Education", "Engineering and Technology", "Environmental and Earth Sciences", "Health and Medicine", "Law and Legal Studies", "Life Sciences and Biology", "Mathematics and Statistics", "Physical Sciences (e.g., Physics, Chemistry)", "Social Sciences", "Other"],
-    "pregunta_experiencia": "How many years of experience do you have in this field?:", 
-    "opciones_experiencia": ["Less than 1 year", "1 - 3 years", "4 - 6 years", "7 - 10 years", "More than 10 years"], 
-    "pregunta_ciudad": "In which country do you reside?",
-    "Gracias_por_contestar_el_formulario": "Thank you so much for answering this form!",
-    
+    "opcion_correo": "Email (optional):",
+    "pregunta_nivel_estudios": "Education Level",
+    "opciones_nivel_estudios": ["Primary education", "Secondary education", "University education (or higher)", "No education", "Other"], 
+    "pregunta_rama_estudios": "Please select the field that best describes your studies:",
+    "opciones_rama_estudios": [
+        "Arts and Humanities (Design, Philosophy, History, Translation and Interpretation, ...)",
+        "Sciences (Environmental Sciences, Physics, Geology, Mathematics, Chemistry, ...)",
+        "Health Sciences (Nursing, Medicine, Psychology, Dentistry, Veterinary, ...)",
+        "Social and Legal Sciences (Business, Audiovisual Communication, Criminology, Law, Economics, Journalism, Tourism, ...)",
+        "Biosciences (Biology, Biochemistry, Genetics, Microbiology, ...)",
+        "Engineering and Architecture (Engineering, Industrial, Computer Science, Telecommunications, ...)",
+        "Other"
+    ],
+    "pregunta_años_experiencia": "How many years of experience do you have in this field?:",
+    "opciones_años_experiencia": ["Less than 1 year", "1 - 3 years", "4 - 6 years", "7 - 10 years", "More than 10 years"],
+    "pregunta_pais_residencia": "In which country do you reside?",
+    "Gracias_por_contestar_el_formulario": "Thank you very much for completing the form!",
+
     # Main texts
-    "navegacion": "Navigation", 
-    "selecciona_una_seccion": "Select a section:", 
-    "contacto": "Contact", 
+    "navegacion": "Navigation",
+    "selecciona_una_seccion": "Select a section:",
+    "contacto": "Contact",
     "sobre_nosotros": "About Us",
-    "info_sobre_nosotros": "This questionnaire is part of the final project from my degree.", 
-    "info_contacto": "If you wish to contact us, please fill out the following form:", 
-    "correo_electronico_contacto": "Email: ", 
-    "mensaje": "Message: ", 
-    "enviar": "Send", 
-    "mensaje_enviado": "Message sent successfully!", 
-    
-    #Errores
-    "error_nombre": "Please enter your name. ",  
-    "error_apellido": "Please enter your surname. ",  
-    "error_genero": "Please enter your gender. ",  
-    "error_correo": "Email address not valid. ",  
-    "error_age": "Please enter your age. ",  
+    "info_sobre_nosotros": "This questionnaire is part of the final project from my degree.",
+    "info_contacto": "If you would like to contact us, please complete the form below:",
+    "correo_electronico_contacto": "Email:",
+    "mensaje": "Message:",
+    "enviar": "Send",
+    "mensaje_enviado": "Message sent successfully!",
+
+    # Errors
+    "error_genero": "Please enter your gender.",
+    "error_correo": "Invalid email address.",
+    "error_age": "Please enter your age.",
+    "error_nivel_estudios": "Please enter your education level.",
+    "error_rama_estudios": "Please enter your field of study.",
+    "error_años_experiencia": "Please enter how many years of experience you have in your field of study.",
+    "error_pais_residencia": "Please enter your country of residence.",
+
+    "video_no_encontrado": "The video was not found at the specified path.",
+    "selecciona_opción": "Please select an option before continuing.",
 }
